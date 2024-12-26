3bcc3239c5d26be58222509a0ed836072b4c7a18
omar shalouf <ben97shallof@gmail.com>
2024-12-26 13:11:50 +0000

    Update app

--- frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_message/whatsapp_message.py
+++ frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_message/whatsapp_message.py
@@ -251,7 +251,7 @@ def send_doc_pdf(to, doctype,docname,print_format):
             "type": "Outgoing",
             "message_type": "Manual",
             "reference_doctype": doctype,
-            "reference_name": docname,
+            "reference_name": title,
             "content_type": "document",
             "attach": pdf_url,
             "label":doctype,
